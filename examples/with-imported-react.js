35be248ff101eca3a6a5de24fe4e1d826be4069b
errorpro <fundus@yandex.ru>
2015-06-19 16:45:16 +0000

    change eslint config

--- examples/with-imported-react.js
+++ examples/with-imported-react.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 export default class Component {
-  static displayName() {}
-
   render() {
     return <div />
   }
--- examples/without-imported-react.js
+++ examples/without-imported-react.js
@@ -1,6 +1,4 @@
 export default class Component {
-  static displayName() {}
-
   render() {
     return <div />
   }

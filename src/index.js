a98439db1be1f01dc43583d6f0132dd4e5179275
Philip Nilsson <phn@trustpilot.com>
2017-03-03 17:37:05 +0100

    Add lib/name flags to support other libraries than react

--- src/index.js
+++ src/index.js
@@ -10,14 +10,18 @@ export default function ({ types: t }) {
           file.set('hasJSX', false);
         },
 
-        exit({ node, scope }, { file }) {
-          if (!(file.get('hasJSX') && !scope.hasBinding('React'))) {
+        exit({ node, scope }, { file, opts }) {
+
+          const name = opts.name || 'React';
+          const lib = opts.lib || name.toLowerCase();
+
+          if (!file.get('hasJSX') || scope.hasBinding(name)) {
             return;
           }
 
           const reactImportDeclaration = t.importDeclaration([
-            t.importDefaultSpecifier(t.identifier('React')),
-          ], t.stringLiteral('react'));
+            t.importDefaultSpecifier(t.identifier(name)),
+          ], t.stringLiteral(lib));
 
           node.body.unshift(reactImportDeclaration);
         },

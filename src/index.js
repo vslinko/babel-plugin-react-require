e914542eea48052f62e18e6b4afb96d6d014aaad
Olegs Jeremejevs <olegs@jeremejevs.com>
2018-04-28 21:31:34 +0300

    Improve interoperability with other plugins

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
 export default function ({ types: t }) {
   return {
     visitor: {
-      JSXOpeningElement(path, { file }) {
-        file.set('hasJSX', true);
-      },
-
       Program: {
         enter(path, { file }) {
-          file.set('hasJSX', false);
-        },
-
-        exit({ node, scope }, { file }) {
-          if (!(file.get('hasJSX') && !scope.hasBinding('React'))) {
-            return;
-          }
-
-          const reactImportDeclaration = t.importDeclaration([
+          const ourNode = t.importDeclaration([
             t.importDefaultSpecifier(t.identifier('React')),
           ], t.stringLiteral('react'));
 
-          node.body.unshift(reactImportDeclaration);
+          // Add an import early, so that other plugins get to see it
+          file.set('ourPath', path.unshiftContainer('body', ourNode)[0]);
         },
+
+        exit(_, { file }) {
+          // If our import is still intact and we haven't encountered any JSX in
+          // the program, then we just remove it. There's an edge case, where
+          // some other plugin could add JSX in its `Program.exit`, so our
+          // `JSXOpeningElement` will trigger only after this method, but it's
+          // likely that said plugin will also add a React import too.
+          const ourPath = file.get('ourPath');
+          if (ourPath && !file.get('hasJSX')) {
+            if (!ourPath.removed) ourPath.remove();
+            file.set('ourPath', undefined);
+          }
+        },
+      },
+
+      ImportDeclaration(path, { file }) {
+        // Return early if this has nothing to do with React
+        if (path.node.specifiers.every(x => x.local.name !== 'React')) return;
+
+        // If our import is still intact and we encounter some other import
+        // which also imports `React`, then we remove ours.
+        const ourPath = file.get('ourPath');
+        if (ourPath && path !== ourPath) {
+          if (!ourPath.removed) ourPath.remove();
+          file.set('ourPath', undefined);
+        }
+      },
+
+      JSXOpeningElement(_, { file }) {
+        file.set('hasJSX', true);
       },
     },
   };

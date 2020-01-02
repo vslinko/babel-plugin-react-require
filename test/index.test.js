5198f486324e9d17be0adc246832d16a05de12c9
Viacheslav Slinko <vslinko@yahoo.com>
2020-01-02 16:05:49 +0300

    Fix interop with babel-plugin-inline-react-svg

--- test/index.test.js
+++ test/index.test.js
@@ -3,8 +3,10 @@ const babel6 = require('babel-core');
 
 const reactPlugin = require('../src/index').default;
 
-function transform(code, pluginsBefore = [], pluginsAfter = []) {
+function transform(code, { pluginsBefore = [], pluginsAfter = [], filename = 'test.js' } = {}) {
   return babel7.transform(code, {
+    filename,
+    cwd: __dirname,
     plugins: ['@babel/plugin-syntax-jsx', ...pluginsBefore, reactPlugin, ...pluginsAfter],
   }).code;
 }
@@ -77,10 +79,10 @@ describe('babel-plugin-react', () => {
   });
 
   it('should get along with other plugins which add React import', () => {
-    expect(transform(genericInput, [somePluginEnter])).toBe(genericOutput);
-    expect(transform(genericInput, [somePluginExit])).toBe(genericOutput);
-    expect(transform(genericInput, [], [somePluginEnter])).toBe(genericOutput);
-    expect(transform(genericInput, [], [somePluginExit])).toBe(genericOutput);
+    expect(transform(genericInput, { pluginsBefore: [somePluginEnter] })).toBe(genericOutput);
+    expect(transform(genericInput, { pluginsBefore: [somePluginExit] })).toBe(genericOutput);
+    expect(transform(genericInput, { pluginsAfter: [somePluginEnter] })).toBe(genericOutput);
+    expect(transform(genericInput, { pluginsAfter: [somePluginExit] })).toBe(genericOutput);
   });
 
   it('should work with babel6', () => {
@@ -92,20 +94,29 @@ describe('babel-plugin-react', () => {
   });
 
   it('should work with other plugins which use scope.crawl on files which already contains React import', () => {
-    const transformed = transform('import * as React from "react";', [], [somePluginThatCrawl]);
+    const transformed = transform('import * as React from "react";', { pluginsAfter: [somePluginThatCrawl] });
 
     expect(transformed).toBe('import * as React from "react";');
   });
 
   it('should not blow up if another plugin removes our import', () => {
-    expect(transform(genericInput, [], [somePluginCrazy])).toBe('export default class Component {\n  render() {\n    return <div />;\n  }\n\n}');
-    expect(transform('const x = 1;', [], [somePluginCrazy])).toBe('const x = 1;');
-    expect(transform('', [somePluginEnter], [somePluginCrazy])).toBe('import React from "react";');
+    expect(transform(genericInput, { pluginsAfter: [somePluginCrazy] })).toBe('export default class Component {\n  render() {\n    return <div />;\n  }\n\n}');
+    expect(transform('const x = 1;', { pluginsAfter: [somePluginCrazy] })).toBe('const x = 1;');
+    expect(transform('', { pluginsBefore: [somePluginEnter], pluginsAfter: [somePluginCrazy] })).toBe('import React from "react";');
   });
 
   it('should support JSX fragments', () => {
     const transformed = transform('function Thing() {\n  return <>Hi</>;\n}');
 
     expect(transformed).toBe('import React from "react";\n\nfunction Thing() {\n  return <>Hi</>;\n}');
+  });
+
+  it('should work with babel-plugin-inline-react-svg', () => {
+    const transformed = transform('import svg from "./test.svg";', {
+      filename: 'test.svg',
+      pluginsAfter: ['inline-react-svg'],
+    });
+
+    expect(transformed).toBe('import React from "react";\n\nvar svg = function svg(props) {\n  return <svg {...props}><path fill="none" stroke="#000" strokeWidth="5" strokeOpacity=".5" d="M0 0h400v400H0z" /></svg>;\n};\n\nsvg.defaultProps = {\n  xmlns: "http://www.w3.org/2000/svg"\n};');
   });
 });
